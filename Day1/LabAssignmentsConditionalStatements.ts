e791f71e67cdb1b1afe87fe16ff19e6d31e3317c
VaishakSKumar <vaishakspostbox@gmail.com>
2026-09-08 13:24:56 +0530

    Playwright Tasks Day 1

---
+++ Day1/LabAssignmentsConditionalStatements.ts
@@ -0,0 +1,22 @@
+                                // Lab Assignments
+// If condition:
+// 1. Check if a character is uppercase.
+// 2. Check if a number is a multiple of 10.
+
+
+// let charc:String='u';
+// if (charc>='A'&&charc<='Z') {
+//     console.log(`${charc} Character is Uppercase`);
+// }
+
+// let number:number=20;
+// if (number%10==0) {
+//     console.log(`${number} is a Multiple of 10`)
+// }
+
+// If else condition:
+// 3. Check if a person is a teenager (age between 13 and 19).
+// 4. Compare two numbers and print the larger one.
+// 5. Check if a number is positive, negative, or zero.
+// 6. Check if a person is eligible for a senior citizen discount (age >= 60).
+
