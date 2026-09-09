573d648c38dd66dced0051f764cab816203e5449
VaishakSKumar <vaishakspostbox@gmail.com>
2026-09-09 18:45:50 +0530

    Adding Break statements

--- Day2/LabAssignmentsLoopingStatements.ts
+++ Day2/LabAssignmentsLoopingStatements.ts
@@ -281,13 +281,29 @@ while (i<=30) {
     i++;               
 }
 }
-
 //-------------------------------------------------------------------------
 // 21. Write a program to find and print the first even number between 1 and 10 using a for loop.
 //  Use the break statement to exit the loop as soon as you find the first even number.
-
+{
+console.log(`Even Numbers From 1 to 10`);  
+for (let i = 1; i <= 10; i++) {
+        if (i % 2 === 0) {
+            console.log(`First even number is: ${i}`);
+            break;
+        }        
+    }
+}
 //-------------------------------------------------------------------------
 // 22. Write a program to print numbers from 1 to 30. 
 // Stop printing and exit the loop when you find a number greater than 15. 
 // Use the break statement within a for loop.
+{
+console.log(`Print Numbers From 1 to 30`);  
+for (let i=1;i<=30; i++) {
+    if (i>15) {
+         break;
+    }        
+    console.log(`${i}`);               
+}
+}
 //-------------------------------------------------------------------------
