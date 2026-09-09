b733df4d56f146ede6a034b7e7adb71da6678a71
VaishakSKumar <vaishakspostbox@gmail.com>
2026-09-09 10:40:43 +0530

    Adding Nested If Code

--- Day1/LabAssignmentsConditionalStatements.ts
+++ Day1/LabAssignmentsConditionalStatements.ts
@@ -2,6 +2,22 @@
 //                          If condition:
 // 1. Check if a character is uppercase.
 // 2. Check if a number is a multiple of 10.
+//                         If else condition:
+// 3. Check if a person is a teenager (age between 13 and 19).
+// 4. Compare two numbers and print the larger one.
+// 5. Check if a number is positive, negative, or zero.
+// 6. Check if a person is eligible for a senior citizen discount (age >= 60).
+//                         Nested if else: 
+// 7. Check if a number is positive and even. 
+// 8. Check if a character is an uppercase vowel. 
+// 9. Find the largest of three numbers. 
+// 10. Check if a number is a multiple of both 5 and 10. 
+// 11. Check if a character is a vowel or consonant. 
+// 12. Check if a number is divisible by both 2 and 3. 
+//                          Switch case: 
+// 13. Print the corresponding month name for a given month number. 
+// 14. Perform basic arithmetic operations based on user input. 
+// 15. Print the season based on the month number. 
 //-------------------------------------------------------------------------
 // 1. Check if a character is uppercase.
 let charc:String='u';
@@ -60,5 +76,85 @@ if (age >=60) {
     console.log(`Person aged ${age} is Eligible for Senior Citizen Discount`);    
 }else{
     console.log(`Person aged ${age} is Not Eligible for Senior Citizen Discount`);    
+}
+//-------------------------------------------------------------------------
+//                         Nested if else: 
+// 7. Check if a number is positive and even. 
+// 8. Check if a character is an uppercase vowel. 
+// 9. Find the largest of three numbers. 
+// 10. Check if a number is a multiple of both 5 and 10. 
+// 11. Check if a character is a vowel or consonant. 
+// 12. Check if a number is divisible by both 2 and 3. 
+//-------------------------------------------------------------------------
+// 7. Check if a number is positive and even. 
+let number_1:number=-33;
+if (number_1>0) {
+    if (number_1%2==0) {
+        console.log(`${number_1} is Positive and even`);        
+    }else{
+        console.log(`${number_1} is Positive and Not Even`); 
+   }
+} else {
+       console.log(`${number_1} is Not Positive Number`);       
+}
+//-------------------------------------------------------------------------
+// 8. Check if a character is an uppercase vowel. 
+let character:string='N';
+if (character>="A"&&character<="Z") {
+    if (character==="A"||character==="E"||character==="I"||character==="O"||character==="U") {
+        console.log(`${character} is an uppercase vowel`);        
+    }else{
+        console.log(`${character} is an uppercase and Not vowel`); 
+   }
+} else {
+       console.log(`${character} is Not uppercase`);       
+}
+//-------------------------------------------------------------------------
+// 9. Find the largest of three numbers. 
+let number_1:number=40;
+let number_2:number=39;
+let number_3:number=62;
+if (number_1>=number_2&&number_1>=number_3) {
+    console.log(`${number_1} is largest of 3 numbers so number_1 is Largest`);    
+} else if (number_2>=number_1&&number_2>=number_3) {
+    console.log(`${number_2} is largest of 3 numbers so number_2 is Largest`);
+}else{
+     console.log(`${number_3} is largest of 3 numbers so number_3 is Largest`);
+}
+//-------------------------------------------------------------------------
+// 10. Check if a number is a multiple of both 5 and 10. 
+let number_1:number=93;
+if (number_1%5==0){
+    if(number_1%10==0) {
+        console.log(`${number_1} is a multiple of both 5 and 10`);    
+    }else{
+          console.log(`${number_1} is a multiple of 5 Not 10`);    
+    }
+}else {
+    console.log(`${number_1} is Not a multiple of 5`);
+}
+//-------------------------------------------------------------------------
+// 11. Check if a character is a vowel or consonant. 
+let character:string='U';
+if (character>="A"&&character<="Z"||character>="a"&&character<="z") {
+    if (character==="A"||character==="E"||character==="I"||character==="O"||character==="U"||character==="a"||character==="e"||character==="i"||character==="o"||character==="u") {
+        console.log(`${character} is a vowel`);        
+    }else{
+        console.log(`${character} is consonant`); 
+   }
+}
+//-------------------------------------------------------------------------
+// 12. Check if a number is divisible by both 2 and 3. 
+let number_1:number=13;
+if (number_1%2==0){
+    if(number_1%3==0) {
+        console.log(`${number_1} is divisible by Both 2 and 3`);    
+    }else{
+          console.log(`${number_1} is divisible by 2 and Not 3`);    
+    }
+}else if(number_1%3==0){
+    console.log(`${number_1} is divisible by 3 and Not 2`);
+}else{
+      console.log(`${number_1} is Not divisible by Both 2 and 3`);
 }
 //-------------------------------------------------------------------------
